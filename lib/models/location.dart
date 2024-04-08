ea6635398f8bbc321055ff58c70510d45796b6ab
242darkman <bvouvou@gmail.com>
2024-04-08 13:58:37 +0200

    feat: define the location model

---
+++ lib/models/location.dart
@@ -0,0 +1,15 @@
+class Location {
+  final String title;
+  final String dateRange;
+  final String pricePerNight;
+  final double rating;
+  final String imageUrl;
+
+  Location({
+    required this.title,
+    required this.dateRange,
+    required this.pricePerNight,
+    required this.rating,
+    required this.imageUrl
+});
+}
