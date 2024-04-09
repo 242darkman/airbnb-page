941d217a4e37dedce439befa64c74ce71fafda8a
242darkman <bvouvou@gmail.com>
2024-04-09 10:32:54 +0200

    fix: migrate TabBar to the main file

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'screens/home.dart';
+import '../widgets/app_bar.dart';
 
 void main() {
   runApp(const MainApp());
@@ -14,7 +15,42 @@ class MainApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: "Airbnb Page Example",
       theme: ThemeData.from(colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink.shade600)),
-      home: HomeScreen(),
+      home: DefaultTabController(
+        length: 5,
+        child: Scaffold(
+          appBar: AppBar(
+            title: const Text("Où partons-nous ?"),
+            bottom: const TabBar(
+              tabAlignment: TabAlignment.start,
+              isScrollable: true,
+              tabs: [
+                Tab(text: 'Campagne', icon: Icon(Icons.house_siding)),
+                Tab(text: 'Sur l\'eau', icon: Icon(Icons.houseboat_outlined)),
+                Tab(text: "Avec vue", icon: Icon(Icons.panorama)),
+                Tab(text: "Bord de mer", icon: Icon(Icons.scuba_diving)),
+                Tab(text: "Ski", icon: Icon(Icons.downhill_skiing)),
+              ],
+            ),
+            actions: <Widget>[
+              IconButton(
+                icon: const Icon(Icons.account_box),
+                color: Colors.pink.shade600,
+                onPressed: () {},
+              ),
+            ],
+          ),
+          body: TabBarView(
+            children: [
+              HomeScreen(),
+              Text('Sur l\'eau'),
+              Text('Avec vue'),
+              Text('Bord de mer'),
+              Text('Ski'),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
+
