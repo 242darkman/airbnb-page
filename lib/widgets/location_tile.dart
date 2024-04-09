d7e137075dc81d3dbb4be715c53b034d8ab927e8
242darkman <bvouvou@gmail.com>
2024-04-09 08:46:26 +0200

    fix: edit location tile to add more information

--- lib/widgets/location_tile.dart
+++ lib/widgets/location_tile.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import '../models/location.dart';
 import 'favorite_button.dart';
+import 'members_favorite.dart';
+
 
 class LocationTile extends StatelessWidget {
   final Location location;
@@ -10,23 +12,40 @@ class LocationTile extends StatelessWidget {
   @override
   Widget build(BuildContext context){
     return Card(
-      child: Column(
+      child: Stack(
         children: <Widget>[
-          Image.asset(location.imageUrl),
-          ListTile(
-            title: Text(location.title),
-            subtitle: Text('${location.dateRange} - ${location.pricePerNight}'),
-            trailing: Row(
-                mainAxisSize: MainAxisSize.min,
-              children: [
-                Icon(Icons.star, color: Colors.amber),
-                Text('${location.rating}'),
-                FavoriteButton(),
-              ],
+          AspectRatio(
+          aspectRatio: 16 / 9,
+            child: Image.asset(
+              location.imageUrl,
+              fit: BoxFit.cover,
             ),
-          )
-        ],
-      ),
+          ),
+          Positioned(
+            bottom: 0, 
+            left: 0, 
+            right: 0, 
+            child: Container(
+              color: Colors.black.withOpacity(0.4),
+              padding: EdgeInsets.all(8),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text('${location.title}', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text('${location.dateRange}', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                  Text('${location.pricePerNight}', style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),),
+                ],
+              ),
+            )
+          ),
+          Positioned(left: 8, top: 8, child: MembersFavorite()),
+          Positioned(
+              top: 8,
+              right: 8,
+              child: FavoriteButton()
+          ),
+        ]
+      )
     );
   }
 }
